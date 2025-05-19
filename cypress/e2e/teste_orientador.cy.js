49c01b3cc4ef6f33feb4d157e8490dec60caa8ad
Gustavo <gustavo.gr697.gm@gmail.com>
2025-05-19 19:29:36 -0300

    1 - teste de criação de alunos

---
+++ cypress/e2e/teste_orientador.cy.js
@@ -0,0 +1,35 @@
+describe("Teste de Orientador", () => {
+    it('cadastro normal com sucesso', () => {
+    let fazerLogin = login()
+    let infos = createUser()
+    cy.get(':nth-child(1) > .sc-bqOYya > .sc-gHjVMF').type("user" + infos[0])
+    cy.get(':nth-child(2) > .sc-bqOYya > .sc-gHjVMF').type("user" + infos[1]  + "@gmail.com")
+    cy.get(':nth-child(3) > .sc-bqOYya > .sc-gHjVMF').type(infos[2])
+    cy.get(':nth-child(4) > .sc-irLvIq > .sc-csKJxZ').click()
+    cy.get('.Toastify__toast-body > :nth-child(2)').should('contain.text', 'Usuário criado com sucesso!')
+  })
+})
+
+
+function login() 
+{
+  cy.visit('https://confianopai.com/adm/projetos')
+  cy.get(':nth-child(2) > .sc-ktwOfi').type("gustavos@")
+  cy.get(':nth-child(3) > .sc-ktwOfi').type("123")
+  cy.get('.sc-csKJxZ').click()
+  cy.get('[href="/adm/novo-usuario"]').click()
+  cy.get('.sc-dsAqUS').select('Orientador')
+}
+
+function createUser()
+{
+  let hora = new Date().getHours().toString()
+  let minuto = new Date().getMinutes().toString()
+  let segundos = new Date().getSeconds().toString()
+  let nome = hora + minuto + segundos
+  let email = hora + minuto + segundos
+  let senha = hora + minuto + segundos
+  let infos = [nome, email, senha]
+
+  return infos
+}
